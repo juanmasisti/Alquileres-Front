44ebe528616ceb534600ce24243d6b5db9a7aa31
Facu <facuc4rr@gmail.com>
2025-06-05 00:05:26 -0300

    Fix error msg

--- src/app/pages/person-profile/person-profile.component.ts
+++ src/app/pages/person-profile/person-profile.component.ts
@@ -203,11 +203,11 @@ export class PersonProfileComponent implements OnInit {
           });
         },
         // Si ocurre un error al eliminar la cuenta
-        error: () => {
+        error: (err) => {
           this.dialog.open(ConfirmModalComponent, {
             data: {
               title: 'Error al eliminar',
-              description: 'Ocurrió un error al intentar eliminar tu cuenta. Intentalo más tarde.',
+              description: err.error.message,
               confirmText: 'Aceptar',
               icon: 'info'
             }
