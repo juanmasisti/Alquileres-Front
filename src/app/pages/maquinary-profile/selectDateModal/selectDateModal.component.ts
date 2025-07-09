20557330cc0eb935ec57ef3c468f5b12e077977e
Emma Akai <EmmaAkaiGit@gmail.com>
2025-07-09 17:04:09 -0300

    feat: improve layout and styling for management and users components

--- src/app/pages/maquinary-profile/selectDateModal/selectDateModal.component.ts
+++ src/app/pages/maquinary-profile/selectDateModal/selectDateModal.component.ts
@@ -3,7 +3,11 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MaquinariaService } from 'src/app/services/maquinaria.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -60,7 +64,13 @@ export class SelectDateModalComponent implements OnInit {
       width: '400px',
       data: {
         title: `¿Está seguro de que desea cambiar el estado de ${this.data.maquinaria.nombre} de ${this.data.currentState} a ${this.data.newState}?`,
-        description: `Se cancelaran todas las reservas ${this.fechaMantenimiento ? `desde ${this.formtDate(new Date())} hasta ${this.formtDate(this.fechaMantenimiento)}` : ''} de la maquinaria`,
+        description: `Se cancelaran todas las reservas ${
+          this.fechaMantenimiento
+            ? `desde ${this.formtDate(new Date())} hasta ${this.formtDate(
+                this.fechaMantenimiento
+              )}`
+            : ''
+        } de la maquinaria`,
         confirmText: 'Sí, cambiar',
         cancelText: 'Cancelar',
       },
@@ -78,30 +88,36 @@ export class SelectDateModalComponent implements OnInit {
           )
           .subscribe({
             next: () => {
-              this.closeModal(true)
+              this.snackBar.open(
+                'Estado actualizado correctamente.',
+                'Cerrar',
+                { duration: 3000 }
+              );
+              this.closeModal(true);
             },
             error: (err) => {
               console.error('Error al actualizar el estado:', err);
-              this.snackBar.open(
-                '❌ Error al actualizar el estado de la maquinaria',
-                'Cerrar',
-                {
-                  duration: 4000,
-                  panelClass: ['error-snackbar'],
-                }
-              );
-              this.closeModal()
+              const mensajeError =
+                '❌ ' + err?.error?.message ||
+                '❌ Error al actualizar el estado de la maquinaria';
+
+              this.snackBar.open(mensajeError, 'Cerrar', {
+                duration: 4000,
+                panelClass: ['error-snackbar'],
+              });
+
+              this.closeModal();
             },
           });
       }
     });
   }
 
-  private formtDate(date:any) {
-    return Intl.DateTimeFormat('es-AR').format(date).toString()
+  private formtDate(date: any) {
+    return Intl.DateTimeFormat('es-AR').format(date).toString();
   }
 
   closeModal(val: boolean = false) {
-    this.dialogRef.close(val)
+    this.dialogRef.close(val);
   }
 }
